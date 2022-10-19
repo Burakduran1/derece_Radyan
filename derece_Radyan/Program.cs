93c8257b0fd1f399f45c8ff05c205926a83f3be0
Burak Can DURAN <BURAK.DURAN44@hotmail.com>
2022-10-19 22:45:26 +0300

    Add project files.

---
+++ derece_Radyan/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Derece değerini giriniz:");
+int derece = Convert.ToInt32(Console.ReadLine());
+ double pi = 3.14;
+
+
+
+int radyan = Convert.ToInt32(derece * pi /180)  ;
+double gradyans = Convert.ToDouble(derece * 200/180) ;
+Console.WriteLine(derece + " derece " + radyan + " radyan'dır." );
+Console.WriteLine(gradyans + " gradyan " + gradyans + " gradyandır ");
+Console.ReadLine();
+
